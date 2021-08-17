a45ad7d0a17509f1bedcca4f17f0547668c7e8e9
Ger Inberg <info@gerinberg.com>
2021-08-17 16:24:03 +0200

    update uploaddata example

--- src/com/tercen/examples/UploadData.java
+++ src/com/tercen/examples/UploadData.java
@@ -1,13 +1,8 @@
 package com.tercen.examples;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-
-import org.apache.commons.io.FileUtils;
 
 import com.tercen.client.impl.TercenClient;
-import com.tercen.model.impl.CSVFileMetadata;
 import com.tercen.model.impl.FileDocument;
 import com.tercen.model.impl.Project;
 import com.tercen.service.ServiceError;
@@ -18,64 +13,35 @@ public class UploadData {
 	private static final String FILE_NAME = "src/main/resources/crabs-long.csv";
 	private static final String TEAM_NAME = "test-team";
 	private static final String PROJECT_NAME = "myproject";
-	private static final String LOCALHOST_URL = "http://127.0.0.1:5402/";
-
-	private static String type = "csv"; // "txt"
+	private static final String LOCALHOST_URL = "http://10.0.2.2:5402/";
+	private static final String DOMAIN = "tercen";
+	private static final String USERNAME = "test";
+	private static final String PASSWORD = "test";
+	
+	private static String type = "zip"; //  either "zip", "csv" or "txt"
 
 	public static void main(String[] args) {
 		TercenClient client = new TercenClient(UploadData.LOCALHOST_URL);
 
 		try {
 			client.userService.connect2("tercen", "test", "test");
-			final Project project = Utils.getTestProject(client, TEAM_NAME, PROJECT_NAME);
+			final Project project = Utils.getProject(client, TEAM_NAME, PROJECT_NAME);
 
 			if (type.equals("csv")) {
-				FileDocument fileDoc = new FileDocument();
-				String[] filenameParts = FILE_NAME.split("/");
-				fileDoc.name = filenameParts[filenameParts.length - 1];
-				fileDoc.projectId = project.id;
-				fileDoc.acl.owner = project.acl.owner;
-				fileDoc.metadata = new CSVFileMetadata();
-				fileDoc.metadata.contentType = "text/csv";
-				fileDoc.metadata.separator = ",";
-				fileDoc.metadata.quote = "\"";
-				fileDoc.metadata.contentEncoding = "iso-8859-1";
-
-				File file = new File(FILE_NAME);
-				byte[] bytes = FileUtils.readFileToByteArray(file);
-
-				FileDocument result = client.fileService.upload(fileDoc, bytes);
+				FileDocument result = Utils.uploadCsvFile(LOCALHOST_URL, TEAM_NAME, PROJECT_NAME, DOMAIN, USERNAME, PASSWORD, FILE_NAME);
 				System.out.println(result.toJson());
-
-//				CSVTask to remove file 
-//				CSVTask task = new CSVTask();
-//				task.state = new InitState();
-//				task.fileDocumentId = result.id;
-//				task.owner = project.acl.owner;
-//				task.projectId = project.id;
-//
-//				Task newTask = client.taskService.create(task);
-//				client.taskService.runTask(newTask.id);
-//				newTask = client.taskService.waitDone(newTask.id);
-//
-//				if (newTask.state instanceof FailedState) {
-//					System.out.println("CSVTask failed:");
-//				} else {
-//					System.out.println("CSVTask completed");
-//				}
-
-				byte[] bytesOutput = client.fileService.download(result.id);
-				if (Arrays.equals(bytes, bytesOutput)) {
-					System.out.println("OK");
-				} else {
-					System.out.println("NOK");
-				}
-			} else {
+				System.out.println("CSV file uploaded");
+			} else if (type.equals("zip")) {
+				FileDocument result = Utils.uploadZipFile(LOCALHOST_URL, TEAM_NAME, PROJECT_NAME, DOMAIN, USERNAME, PASSWORD, FILE_NAME);
+				System.out.println(result.toJson());
+				System.out.println("Zip file uploaded");
+			} else if (type.equals("txt")) {
 				FileDocument file = new FileDocument();
 				file.name = "hello.txt";
 				file.projectId = project.id;
 				file = client.fileService.upload(file, "hello".getBytes());
 				System.out.println(file.toJson());
+				System.out.println("Txt file uploaded");
 			}
 		} catch (ServiceError e) {
 			e.printStackTrace();
--- src/com/tercen/util/Utils.java
+++ src/com/tercen/util/Utils.java
@@ -1,15 +1,34 @@
 package com.tercen.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.commons.io.FileUtils;
 
 import com.tercen.client.impl.TercenClient;
+import com.tercen.model.impl.CSVFileMetadata;
+import com.tercen.model.impl.FileDocument;
 import com.tercen.model.impl.Project;
+import com.tercen.model.impl.ProjectDocument;
 import com.tercen.service.ServiceError;
 
 public class Utils {
+	
+	private static final String Separator = "/";
 
-	public static Project getTestProject(TercenClient client, String teamOrUser, String projectName)
+	private static String getFilename(String fullFileName) {
+		String[] filenameParts = fullFileName.replaceAll(Pattern.quote(Utils.Separator), "\\\\").split("\\\\");
+		return filenameParts[filenameParts.length - 1];
+	}
+
+	public static Project getProject(TercenClient client, String teamOrUser, String projectName)
 			throws ServiceError {
 
 		List startKey = List.of(teamOrUser, false, "2000");
@@ -29,5 +48,82 @@ public class Utils {
 		new_project.acl.owner = teamOrUser;
 
 		return client.projectService.create(new_project);
+	}
+	
+	private static void removeProjectFileIfExists(TercenClient client, Project project, String filename) throws ServiceError {
+		List<ProjectDocument> projectDocs = client.projectDocumentService.findProjectObjectsByLastModifiedDate(null, null, 100, 0, true, false);
+		if (projectDocs.size() > 0) {
+			projectDocs = projectDocs
+				  .stream()
+				  .filter(p -> p.projectId.equals(project.id))
+				  .filter(p -> p.name.equals(filename))
+				  .collect(Collectors.toList());
+			
+			projectDocs.forEach(p ->  {
+				try {
+					client.fileService.delete(p.id, p.rev);
+				} catch (ServiceError e) {
+					e.printStackTrace();
+				}
+			});
+		}
+	}
+	
+	public static FileDocument uploadZipFile(String url, String teamName, String projectName, String domain, String username, String password, String fullFileName) throws ServiceError, IOException {
+		// Write data to tercen
+		TercenClient client = new TercenClient(url);
+		client.userService.connect2(domain, username, password);
+		Project	project = getProject(client, teamName, projectName);
+			
+		FileDocument fileDoc = new FileDocument();
+		String filename = getFilename(fullFileName);
+		int index = filename.lastIndexOf(".");
+        String ext = filename.substring(index);
+		String outFilename =  filename.replace(ext, ".zip");
+		fileDoc.name = outFilename;
+		fileDoc.projectId = project.id;
+		fileDoc.acl.owner = project.acl.owner;
+		fileDoc.metadata.contentType = "application/zip";
+		fileDoc.metadata.contentEncoding = "zip,iso-8859-1";
+		File file = new File(fullFileName);
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ZipOutputStream zos = new ZipOutputStream(bos);
+		ZipEntry entry = new ZipEntry(file.getName());
+		byte[] bytes = FileUtils.readFileToByteArray(file);
+		zos.putNextEntry(entry);
+		zos.write(bytes, 0, bytes.length);
+		zos.closeEntry();
+		zos.close();
+		
+		byte[] zipBytes = bos.toByteArray();
+		bos.close();
+		// remove existing file and upload new file
+		removeProjectFileIfExists(client, project, outFilename);
+		return client.fileService.upload(fileDoc, zipBytes);		
+	}
+	
+	public static FileDocument uploadCsvFile(String url, String teamName, String projectName, String domain, String username, String password, String fullFileName) throws ServiceError, IOException {
+		// Write data to tercen
+		TercenClient client = new TercenClient(url);
+		client.userService.connect2(domain, username, password);
+		Project	project = getProject(client, teamName, projectName);
+			
+		FileDocument fileDoc = new FileDocument();
+		String filename = getFilename(fullFileName);
+		fileDoc.name = filename;
+		fileDoc.projectId = project.id;
+		fileDoc.acl.owner = project.acl.owner;
+		fileDoc.metadata = new CSVFileMetadata();
+		fileDoc.metadata.contentType = "text/csv";
+		fileDoc.metadata.separator = ",";
+		fileDoc.metadata.quote = "\"";
+		fileDoc.metadata.contentEncoding = "iso-8859-1";
+		File file = new File(fullFileName);
+		byte[] bytes = FileUtils.readFileToByteArray(file);
+		
+		// remove existing file and upload new file
+		removeProjectFileIfExists(client, project, filename);
+		return client.fileService.upload(fileDoc, bytes);			
 	}
 }
